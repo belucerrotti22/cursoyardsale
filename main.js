07e4c041ee2b6c299525e2df1a8f511b3685fedc
belucerrotti22 <belencerrotti@gmail.com>
2023-01-26 14:44:55 -0300

    Aparece la descripción de cada producto al tocarlos//Finalizo el curso de JS Práctico

--- main.js
+++ main.js
@@ -3,19 +3,23 @@ const menuDesktop = document.querySelector('.main-container-desktop-menu');
 const menuMobile = document.querySelector('.container-mobile-menu');
 const menuIcon = document.querySelector('.icon-menu');
 const shoppingCartIcon = document.querySelector('.shopping-cart');
-const shoppingCartPage = document.querySelector('.main-container-shopping-cart');
+const shoppingCartPage = document.querySelector('#shopping-cart');
 const flechitaCart = document.getElementById('flechita');
 const productsContainer = document.querySelector('.products');
+const productDescription = document.querySelector('#product-description');
+const buttonClose = document.querySelector('.close');
 
 
 email.addEventListener('click', littleMenu);
 menuIcon.addEventListener('click', mobileMenu);
 shoppingCartIcon.addEventListener('click', CartPage);
 flechitaCart.addEventListener('click', CartPage);
+buttonClose.addEventListener('click', stopShowingDescription);
 
 function littleMenu (event) {
     event.preventDefault();
     shoppingCartPage.classList.add('inactive');
+    productDescription.classList.add('inactive');  
     menuMobile.classList.add('inactive');      
     menuDesktop.classList.toggle('inactive');    
 }
@@ -23,13 +27,15 @@ function littleMenu (event) {
 function mobileMenu () {
     menuDesktop.classList.add('inactive');
     shoppingCartPage.classList.add('inactive');
+    productDescription.classList.add('inactive');  
     menuMobile.classList.toggle('inactive');    
 }
 
 function CartPage (event) {
     event.preventDefault();
     menuDesktop.classList.add('inactive');
-    menuMobile.classList.add('inactive');       
+    menuMobile.classList.add('inactive');  
+    productDescription.classList.add('inactive');     
     shoppingCartPage.classList.toggle('inactive'); 
 }
 
@@ -51,6 +57,14 @@ const productList = [
     {name: 'Headphones', price: '89,99', img: 'https://images.pexels.com/photos/4526407/pexels-photo-4526407.jpeg?auto=compress&cs=tinysrgb&w=600'},
     {name: 'Mac 8gb RAM', price: '189,99', img: 'https://images.pexels.com/photos/376704/pexels-photo-376704.jpeg?auto=compress&cs=tinysrgb&w=600'},
 ];
+
+const descriptionList = [
+    {title: 'Headphones', description: 'The best way to feel the music and share it with friends.'},
+    {title: 'Mac 8gb RAM', description: 'The best quality for work and entertaining.'},
+    {title: 'Plant', description: 'A beatiful way to decore and bring life to your house.'},
+    {title: 'Motorbike 97', description: 'It gets you wherever you wanna go... in a vintage way.'},
+    {title: 'Camera 4K HD', description: 'This is the best camera ever created, everyone should have one of these in their homes. It´s ideal for vacations, trips, holidays. The best way to save your memories forever.'}
+]
 
 mostrarArticulos(productList);
 
@@ -89,5 +103,43 @@ function mostrarArticulos (array){
         productInfo.append(productPrice, productName);
         button.appendChild(imgButton);
         productsContainer.appendChild(productCard);
+
+        productImg.addEventListener('click', () => prepareDescription(productName.innerText, productPrice.innerText, productImg.getAttribute('src')));
     }
+}
+
+function stopShowingDescription() {
+    productDescription.classList.add('inactive');
+}
+
+function description (name) {
+    
+    let i = 0;
+    while (descriptionList[i].title != name){
+        i++;
+    }
+
+    return descriptionList[i].description;
+}
+
+function prepareDescription (name, price, img) {
+
+    let productName = document.querySelector('#name');
+    productName.innerText = name;
+
+    let productPrice = document.querySelector('#price');
+    productPrice.innerText = price;
+
+    let productImg = document.querySelector('#img');
+    productImg.setAttribute('src', img);
+
+    let productDescription = document.querySelector('#description');
+    productDescription.innerText = description(name);
+
+    showDescription();
+}
+
+function showDescription () {
+    shoppingCartPage.classList.add('inactive');
+    productDescription.classList.remove('inactive');
 }
